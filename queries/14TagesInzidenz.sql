6f772d225348171df02323d88f3baa9278116d97
Colazivi <22887623+colazivi@users.noreply.github.com>
2020-09-09 19:13:17 +0200

    Made queries more fault tolerant.

--- queries/14TagesInzidenz.sql
+++ queries/14TagesInzidenz.sql
@@ -1,4 +1,4 @@
-SELECT A.GKZ, C.Bezirk, C.Bundesland, C.Artikel, (CAST(Anzahl_neu AS SIGNED) - CAST(Anzahl_alt AS SIGNED)) AS Differenz_Anzahl, ROUND(Inzidenz_neu - Inzidenz_alt, 1) AS Differenz_Inzidenz, A.Timestamp AS Timestamp_Alt, B.Timestamp AS Timestamp_Neu, TIMESTAMPDIFF(DAY, A.Timestamp, B.Timestamp) AS Intervall FROM
+SELECT B.GKZ, C.Bezirk, C.Bundesland, C.Artikel, (CAST(Anzahl_neu AS SIGNED) - CAST(IFNULL(Anzahl_alt, 0) AS SIGNED)) AS Differenz_Anzahl, ROUND(Inzidenz_neu - IFNULL(Inzidenz_alt, 0.0), 1) AS Differenz_Inzidenz, A.Timestamp AS Timestamp_Alt, B.Timestamp AS Timestamp_Neu, TIMESTAMPDIFF(DAY, A.Timestamp, B.Timestamp) AS Intervall FROM
 
 ( SELECT GKZ, Anzahl as Anzahl_alt, Anzahl_Inzidenz AS Inzidenz_alt, Timestamp FROM Bezirksinfektionen
 
@@ -6,7 +6,7 @@ WHERE Timestamp =
 
 (SELECT MIN(Timestamp) FROM Bezirksinfektionen WHERE Timestamp >= DATE_SUB((SELECT MAX(Timestamp) FROM Bezirksinfektionen), INTERVAL 14 DAY)) ) A
 
-INNER JOIN
+RIGHT JOIN
 (SELECT GKZ, Anzahl AS Anzahl_neu, Anzahl_Inzidenz AS Inzidenz_neu, Timestamp
 
 FROM Bezirksinfektionen
@@ -18,6 +18,6 @@ INNER JOIN
 FROM Bezirke
 INNER JOIN Bezirksinfektionen ON Bezirke.GKZ = Bezirksinfektionen.GKZ
 INNER JOIN Bundesländer ON Bezirke.Bundesland = Bundesländer.GKZ) C
-ON A.GKZ = C.GKZ
+ON B.GKZ = C.GKZ
 
 ORDER BY Differenz_Inzidenz DESC
