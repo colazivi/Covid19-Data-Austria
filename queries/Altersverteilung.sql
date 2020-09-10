ad6b772c682214b9ff9dbefbd378ffa54939dfa2
Colazivi <22887623+colazivi@users.noreply.github.com>
2020-09-10 13:40:37 +0200

    Fixed typos.

--- queries/Altersverteilung.sql
+++ queries/Altersverteilung.sql
@@ -1,4 +1,4 @@
-SELECT DATE(A.Timestamp) AS Datum, TIME(A.Timestamp) AS Uhrzeit, A.Anzahl AS "<5", B.Anzahl AS "5-14", C.Anzahl AS "15-24", D.Anzahl AS "25-34", E.Anzahl AS "35-44", F.Anzahl AS "45-54", G.Anzahl AS "55-64", H.Anzahl AS "65-74", I.Anzahl AS "75-84", J.Anzahl AS ">85" FROM Altersverteilung A
+SELECT DATE(A.Timestamp) AS Datum, TIME(A.Timestamp) AS Uhrzeit, A.Anzahl AS "<5", B.Anzahl AS "5-14", C.Anzahl AS "15-24", D.Anzahl AS "25-34", E.Anzahl AS "35-44", F.Anzahl AS "45-54", G.Anzahl AS "55-64", H.Anzahl AS "65-74", I.Anzahl AS "75-84", J.Anzahl AS ">84" FROM Altersverteilung A
 INNER JOIN Altersverteilung B ON A.Timestamp=B.Timestamp AND B.Altersgruppe=2
 INNER JOIN Altersverteilung C ON A.Timestamp=C.Timestamp AND C.Altersgruppe=3
 INNER JOIN Altersverteilung D ON A.Timestamp=D.Timestamp AND D.Altersgruppe=4
