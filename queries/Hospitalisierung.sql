8b92268f372c057ddd8b8dba38a291d368996c86
Colazivi <22887623+colazivi@users.noreply.github.com>
2020-10-08 18:11:43 +0200

    fixup! Added hospitalisation-data

---
+++ queries/Hospitalisierung.sql
@@ -0,0 +1,5 @@
+SELECT DATE(Timestamp) AS Datum, TIME(Timestamp) AS Uhrzeit, GesNBVerf AS "Verfügbare Normalbetten", GesIBVerf AS "Verfügbare Intensivbetten", GesNBBel AS "Belegte Normalbetten", GesIBBel AS "Belegte Intensivbetten", 100*GesNBBel/(GesNBBel+GesNBVerf) AS "Auslastung Normalbetten", 100*GesIBBel/(GesIBBel+GesIBVerf) AS "Auslastung Intensivbetten"
+
+FROM AllgemeinDaten
+
+ORDER BY Timestamp
