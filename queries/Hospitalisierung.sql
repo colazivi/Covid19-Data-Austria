7b9d58ab8c23da653ffd3955012b01606c76e2fe
Colazivi <22887623+colazivi@users.noreply.github.com>
2020-10-09 14:25:09 +0200

    Daily commit: Updated TSVs

--- queries/Hospitalisierung.sql
+++ queries/Hospitalisierung.sql
@@ -1,4 +1,4 @@
-SELECT DATE(Timestamp) AS Datum, TIME(Timestamp) AS Uhrzeit, GesNBVerf AS "Verfügbare Normalbetten", GesIBVerf AS "Verfügbare Intensivbetten", GesNBBel AS "Belegte Normalbetten", GesIBBel AS "Belegte Intensivbetten", 100*GesNBBel/(GesNBBel+GesNBVerf) AS "Auslastung Normalbetten", 100*GesIBBel/(GesIBBel+GesIBVerf) AS "Auslastung Intensivbetten"
+SELECT DATE(Timestamp) AS Datum, TIME(Timestamp) AS Uhrzeit, GesNBVerf + GesNBBel AS "Gesamte Normalbetten", GesIBVerf + GesIBBel AS "Gesamte Intensivbetten", GesNBVerf AS "Verfügbare Normalbetten", GesIBVerf AS "Verfügbare Intensivbetten", GesNBBel AS "Belegte Normalbetten", GesIBBel AS "Belegte Intensivbetten", 100*GesNBBel/(GesNBBel+GesNBVerf) AS "Auslastung Normalbetten (%)", 100*GesIBBel/(GesIBBel+GesIBVerf) AS "Auslastung Intensivbetten (%)"
 
 FROM AllgemeinDaten
 
